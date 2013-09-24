34fedf02f02a6d5b5e22bce66fdf46afae55539e
Eytan Anjel <anjel@usc.edu>
2013-09-23 22:50:19 -0700

    done

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,11 +1,17 @@
 class TeamsController < ApplicationController
     
-    def index
-        @teams = Team.all
-    end
-    
-    def show
-        @team = Team.find params[:id]
-    end
+  def index
+    @teams = Team.all
+  end
+  
+  # def show
+  #   @team = Team.find params[:id]
+  #   @players = @team.players
+  # end
+  
+  def show
+    @team = Team.find params[:id]
+    @players = @team.players.includes(:contract)
+  end
     
 end
